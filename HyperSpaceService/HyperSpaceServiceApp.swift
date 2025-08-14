7b8ce76c23743e845e3b10407760f2a7b57048c8
Logan Miller <lmille77@whitestar.io>
2025-08-14 16:34:06 -0400

    Initial Commit

---
+++ HyperSpaceService/HyperSpaceServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperSpaceServiceApp.swift
+//  HyperSpaceService
+//
+//  Created by Logan Miller on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HyperSpaceServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
